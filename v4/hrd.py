57a517501931317eb4d1d9304412a2658508e040
Jan Röder <jroeder@users.fias.science>
2018-09-04 14:40:33 +0200

    v4 added

---
+++ v4/hrd.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+from numpy import *
+
+x1 = []
+y1 = []
+
+with open('hrd.dat', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        y1.append(float(row[0]))
+        x1.append(float(row[1]))
+
+plt.plot(x1, y1, 'bo', label='HRD Data')
+
+#plt.title('Altersbestimmung von Sternhaufen\nHertzsprung-Russell-Diagramm')
+plt.ylabel('Helligkeit /mag')
+plt.xlabel('B-V /mag')
+plt.legend(loc=4)
+plt.show()
